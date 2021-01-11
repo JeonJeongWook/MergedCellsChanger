3f11cfe62bf3a037e1a0441d8e86dcad1b9de554
JeongWook Jeon <jjwook324@gmail.com>
2021-01-11 13:22:30 +0900

    gui

---
+++ gui.py
@@ -0,0 +1,32 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QPlainTextEdit
+
+
+class gui(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        ta = QPlainTextEdit(self)
+        ta.setPlaceholderText('내용 붙여넣기')
+
+        btn = QPushButton(self)
+        btn.setText('생성')
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(ta)
+        vbox.addWidget(btn)
+
+        self.setLayout(vbox)
+        self.setWindowTitle('QPushButton')
+        self.setGeometry(300, 300, 300, 200)
+        self.show()
+
+    # def btn_clicked(self):
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = gui()
+    sys.exit(app.exec_())
