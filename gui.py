9ef047755b9be099c4fb76605d15264a7c5504ed
JeonJeongWook <jjwook324@gmail.com>
2021-01-12 22:26:03 +0900

    add change merged cell function

--- gui.py
+++ gui.py
@@ -1,84 +1,90 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QPlainTextEdit, QSpinBox, QMessageBox
 
 import openpyxl
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QPlainTextEdit, QSpinBox, QMessageBox, \
+    QDesktopWidget
 
-class gui(QWidget):
+
+class GUI(QWidget):
     def __init__(self):
         super().__init__()
-        self.initUI()
+        self.init_ui()
 
-    def initUI(self):
+    def init_ui(self):
+        self.col_count = QSpinBox(self)
+        self.textarea = QPlainTextEdit(self)
         vbox = QVBoxLayout()
-        self.colCount = QSpinBox(self)
-        self.ta = QPlainTextEdit(self)
         btn_make = QPushButton(self)
-        # btn_clear
+        btn_clear = QPushButton(self)
         # 병합할 열 개수 | spinbox | 미리보기(A:A, A:B)
 
-        vbox.addWidget(self.colCount)
-        vbox.addWidget(self.ta)
+        vbox.addWidget(self.col_count)
+        vbox.addWidget(self.textarea)
         vbox.addWidget(btn_make)
+        vbox.addWidget(btn_clear)
 
-        self.colCount.setMinimum(1)
+        self.col_count.setMinimum(1)
 
-        self.ta.setPlaceholderText('내용 붙여넣기')
-        self.ta.setPlainText("")
+        self.textarea.setPlaceholderText('내용 붙여넣기')
+        self.textarea.setPlainText("")
 
         btn_make.setText('생성하기')
-        btn_make.clicked.connect(self.makeExcel)
+        btn_make.clicked.connect(self.make_excel)
+
+        btn_clear.setText('지우기')
+        btn_clear.clicked.connect(lambda clear: self.textarea.clear())
 
         self.setLayout(vbox)
         self.setWindowTitle('QPushButton')
-        self.setGeometry(300, 300, 300, 200)
+        self.center()
         self.show()
 
-
     # A 65 / a 97
-    def makeExcel(self):
-        merged_col = self.colCount.value()
-        text = self.ta.toPlainText().strip()
-        row_count = self.ta.document().lineCount()
-
-        print('merged_col \t> ', merged_col)
-        print('text----------\n', text)
-        print("row_count \t> ", row_count)
+    def make_excel(self):
+        merged_col = self.col_count.value()
+        text = self.textarea.toPlainText().strip()
+        row_count = self.textarea.document().lineCount()
 
         if text == "":  # NULL
             QMessageBox.about(self, "오류", '내용을 입력하세요')
         else:  # Not NULL
             print('makeExcel start..')
-            start_col   = 65
-            end_col     = start_col + merged_col - 1
+            start_col = 1
+            end_col = start_col + merged_col - 1
+
+            text = self.textarea.toPlainText().split('\n')
+
             wb = openpyxl.Workbook()
             sheet = wb.active
 
             for i in range(0, row_count):
-                result = chr(start_col) + repr(i+1) + ':' + chr(end_col) + repr(i+1)    # A1:b1
-                idx = str(chr(start_col)+repr(i+1))     #A1
+                sheet.merge_cells(start_row=i + 1, start_column=start_col, end_row=i + 1, end_column=end_col)
+                sheet.cell(row=i + 1, column=1).value = text[i].strip()
 
+            wb.save("./changed_row.xlsx")
+            QMessageBox.about(self, "성공", "파일이 생성되었습니다")
 
-                    '''
-                    # Merging first 3 columns of 1st 3 rows
-                        r1 = 1
-                        r2 = 3
-                        c1 = 1
-                        c2 = 3
-                        sheet.merge_cells(start_row=r1, start_column=c1, end_row=r2, end_column=c2)
-                        https://medium.com/swlh/working-with-spreadsheets-using-python-903202509407
-                    '''
-                # sheet.merge_cells('a1:b1')
-                # sheet['a1'] = "check"
-                sheet.merge_cells(self, result)
-                sheet['a1'] = "check"
+    def center(self):
+        # geometry of the main window
+        qr = self.frameGeometry()
 
-            print('end')
-            wb.save("changed_row.xlsx")
+        # center point of screen
+        cp = QDesktopWidget().availableGeometry().center()
+
+        # move rectangle's center point to screen's center point
+        qr.moveCenter(cp)
+
+        # top left of rectangle becomes top left of window centering it
+        self.move(qr.topLeft())
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = gui()
+    ex = GUI()
     sys.exit(app.exec_())
 
-# pip install openpyxl
-# pip install PyQt5
+'''
+    # pip install openpyxl
+    # pip install PyQt5
+    # https://medium.com/swlh/working-with-spreadsheets-using-python-903202509407
+'''
