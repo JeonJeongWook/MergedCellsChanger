566d63456cf566c61dc47551edaecc807ce459bd
JeongWook Jeon <jjwook324@gmail.com>
2021-01-14 13:14:53 +0900

    refactoring

--- MergeCellsChanger.py
+++ MergeCellsChanger.py
@@ -1,5 +1,4 @@
 import sys
-sys.setrecursionlimit(5000)
 import os.path
 import openpyxl
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QPlainTextEdit, QSpinBox, QMessageBox, \
@@ -33,9 +32,8 @@ class MergeCellsChanger(QWidget):
         btn_make.setText('생성하기')
         btn_make.clicked.connect(self.make_excel)
 
-        btn_clear.setText('지우기')
+        btn_clear.setText('내용 지우기')
         btn_clear.clicked.connect(lambda clear: self.textarea.clear())
-        # btn_clear.clicked.connect(self.delete_excel)
 
         self.setLayout(vbox)
         self.setWindowTitle('QPushButton')
@@ -45,46 +43,42 @@ class MergeCellsChanger(QWidget):
     # A 65 / a 97
     def make_excel(self):
         merged_col = self.col_count.value()
-        text = self.textarea.toPlainText().strip()
-        row_count = self.textarea.document().lineCount()
+        text = self.textarea.toPlainText().strip().split('\n')
+        row_count = self.textarea.document().lineCount()    # 행 개수
 
         if text == "":  # NULL
             QMessageBox.about(self, "오류", '내용을 입력하세요')
-        else:  # Not NULL
+        else:   # Not NULL
             start_col = 1
             end_col = start_col + merged_col - 1
-            text = self.textarea.toPlainText().split('\n')  # 줄바꿈 기준으로 글자 자르기
 
-            # 엑셀
+            # 엑셀 사용
             wb = openpyxl.Workbook()
             sheet = wb.active
 
-            # 시트에 병합된 셀 만들기, 해당 셀에 값 넣기
+            # 시트에 병합된 셀 동적 생성, 해당 셀에 값 넣기
             for i in range(0, row_count):
                 sheet.merge_cells(start_row=i + 1, start_column=start_col, end_row=i + 1, end_column=end_col)
                 sheet.cell(row=i + 1, column=1).value = text[i].strip()
 
-            # 파일 실행중일 때(~$ 임시 파일이 있을 시) 현재 시간으로 파일명 생성
-            # 파일 저장 형식[MCC_yyMMdd_hhmmss]]
-            filename = ".\\MCC_Folder\\MCC.xlsx"
-            tmp_filename = ".\\MCC_Folder\\~$MCC.xlsx"
+            folder_path     = ".\\MCC_Folder\\"
+            filename        = folder_path + "MCC.xlsx"  # .\\MCC_Folder\\MCC.xlsx
+            tmp_filename    = folder_path + "~$MCC.xlsx"
 
-            if os.path.isdir(".\\MCC_Folder"):
-                print('폴더 존재')
-                if os.path.isfile(tmp_filename):  # 임시파일 있으면(MCC 파일 실행중이면)
-                    now = self.get_time()
-                    filename = ".\\MCC_Folder\\MCC_" + str(now) + ".xlsx"
-            else:
-                print('폴더 없음 생성함')
-                os.mkdir(".\\MCC_Folder")
+            if os.path.isdir(folder_path):  # 경로에 폴더 있을 시
+                if os.path.isfile(tmp_filename):    # 파일 실행중일 때(~$ 임시 파일이 있을 시)
+                    now = self.get_time()   # 현재시간
+                    filename = folder_path + "MCC_%s.xlsx" % now    # 현재 시간으로 파일명 생성
+            else:   # 경로에 폴더 없을 시 폴더 생성
+                os.mkdir(folder_path)
+
             wb.save(filename)
-
             QMessageBox.about(self, "성공", "파일이 생성되었습니다")
 
     # 파일 저장할때 날짜 형식 가져오는 함수
     def get_time(self):
         now = time.strftime('%y%m%d_%H%M%S')
-        return now
+        return str(now)
 
     # gui 중앙 위치
     def center(self):
